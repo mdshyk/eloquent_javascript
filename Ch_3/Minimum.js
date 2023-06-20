cdc80638e2f4fdbb9fd3bb586a74cdd82db059f5
Kinza <kinzasheikhsiddiqui@gmail.com>
2023-06-21 01:59:34 +0500

    Minimum done

--- Ch_3/Minimum.js
+++ Ch_3/Minimum.js
@@ -1,1 +1,7 @@
 // Write a function min that takes two arguments and returns their minimum
+
+const min = function(num1, num2) {
+    return Math.min(num1, num2)
+}
+
+console.log(min(5, 3))
--- Ch_3/Recursion.js
+++ Ch_3/Recursion.js
@@ -1,1 +1,13 @@
 // Write a function that takes in a positive integer and return wether it is even or not in boolean
+function isEven(num) {
+    if(num = 0){
+        return true
+    }
+    else if(num % 2 == 0) {
+        return true;
+    } else {
+        return false
+    }
+}
+
+console.log(isEven(75))
