79b666f0c216942f0160996a5d0c8da936bad428
Mahadullah Shaikh <47981279+mdshyk@users.noreply.github.com>
2025-02-10 13:40:11 +0500

    Update Recursion.js

--- Ch_3/Recursion.js
+++ Ch_3/Recursion.js
@@ -1,11 +1,12 @@
 // Write a function that takes in a positive integer and return wether it is even or not in boolean
 function isEven(num) {
-    if(num == 0){
-        return true
-    }
-    if(num == 1) {
-        return false;
-    }
+    //will work the same even if we don't specify these two conditions separately
+    // if(num == 0){
+    //     return true
+    // }
+    // if(num == 1) {
+    //     return false;
+    // }
     if(num % 2 == 0) {
         return true;
     } else {
